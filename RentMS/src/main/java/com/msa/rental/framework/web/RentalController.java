1db548040c3da3181c7f117f8007f34249273783
Jihun Park <jihunpark.tech@gmail.com>
2024-03-28 15:11:28 +0900

    [RentMS] refactor: api adaptor

--- RentMS/src/main/java/com/msa/rental/framework/web/RentalController.java
+++ RentMS/src/main/java/com/msa/rental/framework/web/RentalController.java
@@ -23,56 +23,56 @@ public class RentalController {
     private final ClearOverdueItemUseCase clearOverdueItemUsecase;
     private final InquiryUsecase inquiryUsecase;
 
-    @ApiOperation(value = "도서카드 생성",notes = "사용자정보 -> 도서카드정보")
+    @ApiOperation(value = "도서카드 생성", notes = "사용자정보 -> 도서카드정보")
     @PostMapping("/RentalCard/")
-    public ResponseEntity<RentalCardOutputDTO> creatRentalCard(@RequestBody UserInputDTO userInputDTO)
-    {
+    public ResponseEntity<RentalCardOutputDTO> creatRentalCard(@RequestBody UserInputDTO userInputDTO) {
         RentalCardOutputDTO createRentalCard = createRentalCardUsecase.createRentalCard(userInputDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(createRentalCard);
     }
 
-    @ApiOperation(value = "도서카드 조회",notes = "사용자정보(id) -> 도서카드정보")
+    @ApiOperation(value = "도서카드 조회", notes = "사용자정보(id) -> 도서카드정보")
     @GetMapping("/RentalCard/{userId}")
-    public ResponseEntity<RentalCardOutputDTO> getRentalCard(@PathVariable String userId){
+    public ResponseEntity<RentalCardOutputDTO> getRentalCard(@PathVariable String userId) {
         Optional<RentalCardOutputDTO> rentalCard = inquiryUsecase.getRentalCard(new UserInputDTO(userId, ""));
         return ResponseEntity.ok(rentalCard.get());
     }
-    @ApiOperation(value = "대여도서목록 조회",notes = "사용자정보(id) -> 대여도서목록 조회")
+
+    @ApiOperation(value = "대여도서목록 조회", notes = "사용자정보(id) -> 대여도서목록 조회")
     @GetMapping("/RentalCard/{userId}/rentbook")
-    public ResponseEntity<List<RentItemOutputDTO>> getAllRentItem(@PathVariable String userId){
+    public ResponseEntity<List<RentItemOutputDTO>> getAllRentItem(@PathVariable String userId) {
         Optional<List<RentItemOutputDTO>> allRentItem = inquiryUsecase.getAllRentItem(new UserInputDTO(userId, ""));
         return ResponseEntity.ok(allRentItem.get());
     }
 
-    @ApiOperation(value = "반납도서목록 조회",notes = "사용자정보(id) -> 반납도서목록 조회")
+    @ApiOperation(value = "반납도서목록 조회", notes = "사용자정보(id) -> 반납도서목록 조회")
     @GetMapping("/RentalCard/{userId}/returnbook")
-    public ResponseEntity<List<ReturnItemOutputDTO>> getAllReturnItem(@PathVariable String userId){
+    public ResponseEntity<List<ReturnItemOutputDTO>> getAllReturnItem(@PathVariable String userId) {
         Optional<List<ReturnItemOutputDTO>> allReturnItem = inquiryUsecase.getAllReturnItem(new UserInputDTO(userId, ""));
         return ResponseEntity.ok(allReturnItem.get());
     }
 
-    @ApiOperation(value = "대여기능",notes = "사용자정보,아이템정보1 -> 도서카드정보 ")
+    @ApiOperation(value = "대여기능", notes = "사용자정보,아이템정보1 -> 도서카드정보 ")
     @PostMapping("/RentalCard/rent")
     public ResponseEntity<RentalCardOutputDTO> rentItem(@RequestBody UserItemInputDTO userItemInputDTO) throws Exception {
         RentalCardOutputDTO rentalCardOutputDTO = rentItemUsecase.rentItem(userItemInputDTO);
         return ResponseEntity.ok(rentalCardOutputDTO);
     }
 
-    @ApiOperation(value = "반납기능",notes = "사용자정보,아이템정보1 -> 도서카드정보 ")
+    @ApiOperation(value = "반납기능", notes = "사용자정보,아이템정보1 -> 도서카드정보 ")
     @PostMapping("/RentalCard/return")
     public ResponseEntity<RentalCardOutputDTO> returnItem(@RequestBody UserItemInputDTO userItemInputDTO) throws Exception {
         RentalCardOutputDTO rentalCardOutputDTO = returnItemUsercase.returnItem(userItemInputDTO);
         return ResponseEntity.ok(rentalCardOutputDTO);
     }
 
-    @ApiOperation(value = "연체기능",notes = "사용자정보,아이템정보1 -> 도서카드정보 ")
+    @ApiOperation(value = "연체기능", notes = "사용자정보,아이템정보1 -> 도서카드정보 ")
     @PostMapping("/RentalCard/overdue")
     public ResponseEntity<RentalCardOutputDTO> overdueItem(@RequestBody UserItemInputDTO userItemInputDTO) throws Exception {
         RentalCardOutputDTO rentalCardOutputDTO = overdueItemUsercase.overDueItem(userItemInputDTO);
         return ResponseEntity.ok(rentalCardOutputDTO);
     }
 
-    @ApiOperation(value = "연체해제기능",notes = "사용자정보,포인트 -> 도서카드정보 ")
+    @ApiOperation(value = "연체해제기능", notes = "사용자정보,포인트 -> 도서카드정보 ")
     @PostMapping("/RentalCard/clearoverdue")
     public ResponseEntity<RentalResultOutputDTO> clearOverdueItem(@RequestBody ClearOverdueInfoDTO clearOverdueInfoDTO) throws Exception {
         RentalResultOutputDTO rentalResultOuputDTO = clearOverdueItemUsecase.clearOverdue(clearOverdueInfoDTO);
