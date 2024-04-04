e6c4e2bc2abf060e3550379788e1923c17726779
Jihun Park <jihunpark.tech@gmail.com>
2024-04-04 09:17:16 +0900

    [Event] refactor: Member Event Consumer

--- MemberMS/src/main/java/com/msa/member/framework/kafkaadapter/MemberEventConsumers.java
+++ MemberMS/src/main/java/com/msa/member/framework/kafkaadapter/MemberEventConsumers.java
@@ -5,6 +5,7 @@ import com.msa.member.application.usecase.SavePointUseCase;
 import com.msa.member.application.usecase.UsePointUseCase;
 import com.msa.member.domain.model.event.*;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,29 +14,31 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberEventConsumers {
 
-    private final Logger log = LoggerFactory.getLogger(MemberEventConsumers.class);
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final SavePointUseCase savePointUsecase;
     private final UsePointUseCase usePointUsecase;
     private final MemberEventProducer eventProducer;
 
-    @KafkaListener(topics="${consumer.topic1.name}",groupId = "${consumer.groupid.name}")
+    @KafkaListener(topics = "${consumer.topic1.name}", groupId = "${consumer.groupid.name}")
     public void consumeRent(ConsumerRecord<String, String> record) throws IOException {
-        System.out.printf("rental_rent:"+ record.value());
+        log.info("rental_rent: {}", record.value());
         ItemRented itemRented = objectMapper.readValue(record.value(), ItemRented.class);
-        savePointUsecase.savePoint(itemRented.getIdName(),itemRented.getPoint());
+        savePointUsecase.savePoint(itemRented.getIdName(), itemRented.getPoint());
     }
-    @KafkaListener(topics="${consumer.topic2.name}",groupId = "${consumer.groupid.name}")
-    public void consumeReturn(ConsumerRecord<String, String> record) throws IOException{
-        System.out.printf("rental_return:"+ record.value());
+
+    @KafkaListener(topics = "${consumer.topic2.name}", groupId = "${consumer.groupid.name}")
+    public void consumeReturn(ConsumerRecord<String, String> record) throws IOException {
+        log.info("rental_return: {}", record.value());
         ItemReturned itemReturned = objectMapper.readValue(record.value(), ItemReturned.class);
-        savePointUsecase.savePoint(itemReturned.getIdName(),itemReturned.getPoint());
+        savePointUsecase.savePoint(itemReturned.getIdName(), itemReturned.getPoint());
     }
-    @KafkaListener(topics="${consumer.topic3.name}",groupId = "${consumer.groupid.name}")
+
+    @KafkaListener(topics = "${consumer.topic3.name}", groupId = "${consumer.groupid.name}")
     public void consumeClear(ConsumerRecord<String, String> record) throws Exception {
         OverdueCleared overdueCleared = objectMapper.readValue(record.value(), OverdueCleared.class);
 
@@ -45,19 +48,16 @@ public class MemberEventConsumers {
         eventResult.setPoint(overdueCleared.getPoint());
 
         System.out.printf(record.value());
-        try
-        {
-            usePointUsecase.userPoint(overdueCleared.getIdName(),overdueCleared.getPoint());
+        try {
+            usePointUsecase.userPoint(overdueCleared.getIdName(), overdueCleared.getPoint());
             eventResult.setSuccessed(true);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             eventResult.setSuccessed(false);
         }
         eventProducer.occurEvent(eventResult);
     }
 
-    @KafkaListener(topics="${consumer.topic4.name}",groupId = "${consumer.groupid.name}")
+    @KafkaListener(topics = "${consumer.topic4.name}", groupId = "${consumer.groupid.name}")
     public void consumeUsePoint(ConsumerRecord<String, String> record) throws Exception {
         System.out.printf(record.value());
         PointUseCommand pointUseCommand = objectMapper.readValue(record.value(), PointUseCommand.class);
